7f213cce7065554a8127db68a245815957c5e6df
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-11-08 16:02:48 +0700

    mengubah nama theme menjadi nama project + theme

--- app/src/main/java/com/example/tugasnavigasi/MainActivity.kt
+++ app/src/main/java/com/example/tugasnavigasi/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
 import com.example.tugasnavigasi.ui.theme.TugasNavigasiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +18,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TugasNavigasiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DataApp()
                 }
             }
         }
@@ -32,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    androidx.compose.material3.Text(
         text = "Hello $name!",
         modifier = modifier
     )
