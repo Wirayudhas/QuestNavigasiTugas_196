d3f4e2086642ded795d834aa77ab7c6c36cd1d83
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-11-08 15:40:36 +0700

    menampilkan tampilan UI berisi teks "Selamat Datang" dan gambar dengan deskripsi serta tombol "Masuk"

--- app/src/main/java/com/example/tugasnavigasi/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/WelcomeScreen.kt
@@ -1,2 +1,53 @@
 package com.example.tugasnavigasi.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import com.example.tugasnavigasi.R
+
+@Composable
+fun WelcomeScreen(onMasuk: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "Selamat Datang",
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.6f)
+                )
+                Spacer(Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+
+                Image(
+                    painter = painterResource(id = R.drawable.Roblox),
+                    contentDescription = "Roblox mancing",
+                    modifier = Modifier
+                        .fillMaxWidth(0.75f)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Fit
+                )
+            }
+
+            Button(
+                onClick = onMasuk,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Masuk")
+            }
+        }
+    }
+}
