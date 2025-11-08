0738f22acc5d8d1cab54d5f2cdca03fb07fd2fed
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-11-08 12:30:00 +0700

    menambahkan library pada FormIsian.kt

---
+++ app/src/main/java/com/example/tugasnavigasi/view/FormIsian.kt
@@ -0,0 +1,13 @@
+package com.example.tugasnavigasi.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.tugasnavigasi.R
+
+
