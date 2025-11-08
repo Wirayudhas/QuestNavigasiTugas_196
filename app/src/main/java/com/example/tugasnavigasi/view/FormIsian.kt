dce6980eae89cac15916dc3b016144eb073b756f
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-11-08 12:45:47 +0700

    menambahkkan fungsi yang didalamnya ada 3 lambda expression, pada onSubmit diberi keterangan membawa 4 string

--- app/src/main/java/com/example/tugasnavigasi/view/FormIsian.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/FormIsian.kt
@@ -10,4 +10,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.tugasnavigasi.R
 
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    onSubmit: (String, String, String, String) -> Unit,
+    onSelesaiDialog: () -> Unit,
+    onBeranda: () -> Unit
+)
